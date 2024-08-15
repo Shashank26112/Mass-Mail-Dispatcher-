a23f00d57ffc7f600185c8da8ed632783c233706
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2024-08-15 22:10:31 +0530

    Add files via upload

---
+++ WEB DEVELOPMENT PROJECT FILES ( AJIT KUMAR )/python test questions.py
@@ -0,0 +1,7 @@
+x = int(input("enter the number"))
+y = int(input("enter the number"))
+if  (x%2 == 0):
+ while x < y:
+  print("x is less than y")
+else:
+ print("x is greater than y")
---
+++ WEB DEVELOPMENT PROJECT FILES ( AJIT KUMAR )/readme.txt
@@ -0,0 +1,5 @@
+The templates folder contains the html source code.
+
+To run the project install python IDE like pycharm, install flask framework.
+
+then run the "mail_app.py" file.
